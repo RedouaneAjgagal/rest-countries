f8aea72e324bca981f45efa4e760895b7c5802c1
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-02-13 23:23:23 +0100

    Setup react-router pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './pages/Root';
+import Home from './pages/Home';
+import Country from './pages/Country';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: ':countryName',
+          element: <Country />
+        }
+      ]
+    }
+  ])
+
   return (
-    <div className="App">
-      <h1>Welcome</h1>
+    <div>
+      <RouterProvider router={router} />
     </div>
   )
 }
