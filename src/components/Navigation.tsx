bb47d13ce8ef5483031f3661339caa1b25a3c59c
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-02-14 03:10:09 +0100

    Add Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
-  return (
-    <nav>Navigation</nav>
-  )
+    return (
+        <nav className='flex justify-between items-center px-4 py-8 bg-gray-700'>
+            <Link to={'/'} className='font-semibold tracking-wide'>Where in the world?</Link>
+            <button>Dark Mode</button>
+        </nav>
+    )
 }
 
 export default Navigation
